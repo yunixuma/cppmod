b3c82ab1bb917243e1deb78afe7e0e3e327543c1
Yoshihiro Kosaka <ykosaka@c1r27s7.42tokyo.jp>
2023-03-20 02:30:13 +0900

    Add classes, mkdir

---
+++ ex01/src/Contact.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 15:04:04 by ykosaka           #+#    #+#             */
+/*   Updated: 2023/03/20 02:14:15 by ykosaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "contact.hpp"
+
+class Contact
+{
+private:
+    
+public:
+    Contact();
+    ~Contact();
+}
+
+Contact::Contact(string) {
+    
+}
+
+Contact::~Contact(void) {
+    
+}
---
+++ ex01/src/PhoneBook.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/16 15:04:04 by ykosaka           #+#    #+#             */
+/*   Updated: 2023/03/20 02:14:07 by ykosaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+class Contact
+{
+private:
+    
+public:
+    PhoneBook();
+    ~PhoneBook();
+}
+
+PhoneBook::PhoneBook(string) {
+    
+}
+
+PhoneBook::~PhoneBook(void) {
+    
+}
