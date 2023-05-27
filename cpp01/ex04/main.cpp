fadf4a7ad55d83edb42fab5f7e78535d09953b6f
ykosaka <ykosaka@student.42tokyo.jp>
2023-05-27 19:49:56 +0900

    Update ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,21 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ykosaka <ykosaka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:04:04 by ykosaka           #+#    #+#             */
-/*   Updated: 2023/05/25 17:33:45 by ykosaka          ###   ########.fr       */
+/*   Updated: 2023/05/27 17:43:12 by ykosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
-#include <utility>
+#include <iostream>
+//#include <utility>
 #include <string>
  
 int main()
 {
-	std::string filename = "example.txt";
+	std::string filename = "Makefile";
+	try {
+	//	std::ifstream fs;
+	//	fs.open(filename.c_str(), std::ios::in);
 	std::ifstream fs(filename, std::ios::in);
-    fs.replace
+		if (fs.fail())
+			throw (std::exception());
+	}
+	catch (const std::exception& e) {
+		std::cerr << "\033[31m!!! Error opening the file. !!!\033[m" << std::endl;
+		return (1);
+	}
+	std::string line_buffer;
+	while (std::getline (fs, line_buffer)) {
+	    std::cout << line_buffer << std::endl;
+	}
 	return (0);
+
 }
