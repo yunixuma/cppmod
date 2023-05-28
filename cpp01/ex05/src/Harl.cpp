0925100db026a12ff2ca6dfa70932e6dd4f09c4e
ykosaka <ykosaka@student.42tokyo.jp>
2023-05-29 00:25:28 +0900

    cpp01/ex05

---
+++ cpp01/ex05/src/Harl.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
+/*   Updated: 2023/05/29 00:22:40 by ykosaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+const std::string Harl::levels_[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+Harl::Harl() {
+	std::cout << "\033[36;2;3m" << "Creating a Harl\033[m" << std::endl;
+	pfs_[0] = &Harl::debug;
+	pfs_[1] = &Harl::info;
+	pfs_[2] = &Harl::warning;
+	pfs_[3] = &Harl::error;
+}
+
+Harl::~Harl(void) {
+	std::cout << "\033[31;2;3m" << "Destroying the Harl\033[m" << std::endl;
+}
+
+void	Harl::debug( void ) {
+	const static std::string	msg_debug = "\033[35mI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\033[m";
+	std::cout << msg_debug << std::endl;
+}
+
+void	Harl::info( void ) {
+	const static std::string	msg_info = "\033[36mI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\033[m";
+	std::cout << msg_info << std::endl;
+}
+
+void	Harl::warning( void ) {
+	const static std::string	msg_warning = "\033[33mI think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\033[m";
+	std::cout << msg_warning << std::endl;
+}
+
+void	Harl::error( void ) {
+	const static std::string	msg_error = "\033[31mThis is unacceptable! I want to speak to the manager now.\033[m";
+	std::cout << msg_error << std::endl;
+}
+
+void	Harl::complain( std::string level ) {
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->levels_[i] == level)
+		{
+			std::cout << "\033[2;3mCalling " << &pfs_[i] << " for " << this->levels_[i] << "\033[m" << std::endl;
+			(this->*pfs_[i])();
+		}
+	}
+}
---
+++ cpp01/ex05/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
+/*   Updated: 2023/05/29 00:23:56 by ykosaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return (0);
+}
