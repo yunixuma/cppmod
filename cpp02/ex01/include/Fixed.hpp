b112532fde63cb0128eb6fc08433960a185bd365
ykosaka <ykosaka@student.42tokyo.jp>
2023-05-29 11:46:03 +0900

    Add cpp02/ex01

---
+++ cpp02/ex01/include/Fixed.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
+/*   Updated: 2023/05/29 11:41:09 by ykosaka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+private:
+	int					fixedRawBits_;
+	static const int	fractionalBits_ = 8;
+public:
+	Fixed();
+	Fixed(const int d);
+	Fixed(const float f);
+	Fixed(const Fixed& fixed);
+	Fixed&	operator=(const Fixed& fixed);
+	~Fixed();
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+};
+
+std::ostream&	operator<<(std::ostream& stream, const Fixed& fixed);
+
+#endif
