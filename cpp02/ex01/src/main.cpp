7b17444958588d008093953211572784871f9f5b
ykosaka <ykosaka@student.42tokyo.jp>
2023-05-30 15:46:15 +0900

    Modify cpp02/ex01 (add more tests)

--- cpp02/ex01/src/main.cpp
+++ cpp02/ex01/src/main.cpp
@@ -6,26 +6,96 @@
 /*   By: ykosaka <ykosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
-/*   Updated: 2023/05/29 11:34:33 by ykosaka          ###   ########.fr       */
+/*   Updated: 2023/05/30 15:45:30 by ykosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <iomanip>
 
 int	main( void ) {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
-	a = Fixed( 1234.4321f );
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	{
+		Fixed a;
+		Fixed const b( 10 );
+		Fixed const c( 42.42f );
+		Fixed const d( b );
+		a = Fixed( 1234.4321f );
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	}
+
+	std::cout << "\033[35;43mAdditional tests\033[m" << std::endl;
+	std::cout << "\033[33mNegative numbers\033[m" << std::endl;
+	{
+		Fixed a;
+		Fixed const b( -10 );
+		Fixed const c( -42.42f );
+		Fixed const d( b );
+		a = Fixed( -1234.4321f );
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	}
+	{
+		std::cout << "\033[33mOverflow\033[m" << std::endl;
+		Fixed a;
+		Fixed b( 2147483647 );
+		Fixed c( -2147483648.f / 4 );
+		Fixed d( b );
+		a = Fixed( 2147483647.f / 4 );
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+
+		std::cout << "\033[33mMAX. & min.\033[m" << std::endl;
+		a.setRawBits(1);
+		b.setRawBits(2147483647);
+		c.setRawBits(-1);
+		d.setRawBits(-2147483648);
+		std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+		std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+		std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+		std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "\033[33mSet precision to 12 digits\033[m" << std::endl;
+		std::cout << std::setprecision(10);
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+
+		std::cout << "\033[33mSemi-MAX. & semi-min.\033[m" << std::endl;
+		a.setRawBits(2);
+		b.setRawBits(2147483647 - 64);
+		c.setRawBits(-2);
+		d.setRawBits(-2147483648 + 1 + 64);
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+
+		std::cout << "\033[33mEvaluating the precision\033[m" << std::endl;
+		a.setRawBits(1000000 * 256);
+		b.setRawBits(1000000 * 256 + 9);
+		c.setRawBits(-1000000 * 256);
+		d.setRawBits(-1000000 * 256 - 9);
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c is " << c << std::endl;
+		std::cout << "d is " << d << std::endl;
+	}
+
 	return 0;
 }
