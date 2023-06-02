ebc48d9aacd31e6b2a595d4076b5c9f4a0d2d9a4
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2023-06-02 14:32:24 +0900

    Modify cpp02/ex00 (cp from ex01)

--- cpp03/ex00/include/ClapTrap.hpp
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ykosaka <ykosaka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
-/*   Updated: 2023/06/02 10:55:42 by ykosaka          ###   ########.fr       */
+/*   Updated: 2023/06/02 14:12:28 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,21 @@ private:
 	unsigned int	energyPoint_;
 	unsigned int	attackDamage_;
 public:
-	ClapTrap(std::string name = "You");
+	ClapTrap(std::string name = "CL4P-TP");
 	ClapTrap(const ClapTrap& src);
 	ClapTrap&	operator=(const ClapTrap& rhs);
 	~ClapTrap();
-	void	attack(const std::string& target = "something enemy");
+	void	attack(const std::string& target = "monster");
 	void	takeDamage(unsigned int amount = 1);
 	void	beRepaired(unsigned int amount = 1);
 	const std::string&	getName(void) const;
 	unsigned int	getHitPoint(void) const;
 	unsigned int	getEnergyPoint(void) const;
 	unsigned int	getAttackDamage(void) const;
+	void	setName(const std::string& name);
+	void	setHitPoint(unsigned int hitPoint);
+	void	setEnergyPoint(unsigned int energyPoint);
+	void	setAttackDamage(unsigned int attackDamage);
 };
 
 #endif
