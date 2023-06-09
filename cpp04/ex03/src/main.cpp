6c1c441732d8135da1a62f01d0be0fdff3b3d89a
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2023-06-10 04:14:33 +0900

    Modify cpp04/ex03 (output of main)

--- cpp04/ex03/src/main.cpp
+++ cpp04/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:04:04 by ykosaka           #+#    #+#             */
-/*   Updated: 2023/06/10 04:03:06 by Yoshihiro K      ###   ########.fr       */
+/*   Updated: 2023/06/10 04:11:44 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main()
 		std::cerr.rdbuf(strbuf);
 	}
 
-	std::cout << "\033[35;43mSample test\033[m" << std::endl;
+	std::clog << "\033[35;43mSample test\033[m" << std::endl;
 	{
 		IMateriaSource* src = new MateriaSource();
 		src->learnMateria(new Ice());
@@ -50,36 +50,36 @@ int	main()
 		delete src;
 	}
 
-	std::cout << "\033[35;43mTest for full or empty slot or store\033[m" << std::endl;
+	std::clog << "\033[35;43mTest for full or empty slot or store\033[m" << std::endl;
 	{
-		std::cout << "\033[33mLaunch a default name character\033[m" << std::endl;
+		std::clog << "\033[33mLaunch a default name character\033[m" << std::endl;
 		ICharacter* me = new Character();
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		IMateriaSource* src = new MateriaSource();
 		src->learnMateria(new Ice());
 
-		std::cout << "\033[33mTrying to copy & equip a Materia not learned yet\033[m" << std::endl;
+		std::clog << "\033[33mTrying to copy & equip a Materia not learned yet\033[m" << std::endl;
 		AMateria* tmp;
 		tmp = src->createMateria("cure");
 		me->equip(tmp);
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		src->learnMateria(new Ice());
 		src->learnMateria(new Ice());
 		src->learnMateria(new Ice());
 
-		std::cout << "\033[33mStore is now full, so Materia Materias cannot be learned\033[m" << std::endl;
+		std::clog << "\033[33mStore is now full, so Materia Materias cannot be learned\033[m" << std::endl;
 		src->learnMateria(new Cure());
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		ICharacter* rival = new Character("Golgo XIII");
 
-		std::cout << "\033[33mTrying to use an empty or invalid slot\033[m" << std::endl;
+		std::clog << "\033[33mTrying to use an empty or invalid slot\033[m" << std::endl;
 		me->use(3, *rival);
 		me->use(5, *rival);
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		tmp = src->createMateria("ice");
 		me->equip(tmp);
 		tmp = src->createMateria("ice");
@@ -91,29 +91,29 @@ int	main()
 		tmp = src->createMateria("ice");
 		rival->equip(tmp);
 
-		std::cout << "\033[33mSlot is now full, so Materia Materias cannot be equipped\033[m" << std::endl;
+		std::clog << "\033[33mSlot is now full, so Materia Materias cannot be equipped\033[m" << std::endl;
 		tmp = src->createMateria("ice");
 		me->equip(tmp);
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		me->unequip(0);
 
-		std::cout << "\033[33mUnequip Materias\033[m" << std::endl;
+		std::clog << "\033[33mUnequip Materias\033[m" << std::endl;
 		me->unequip(1);
 		me->unequip(2);
 		me->unequip(3);
 
-		std::cout << "\033[33mTrying to unequip an empty or invalid slot\033[m" << std::endl;
+		std::clog << "\033[33mTrying to unequip an empty or invalid slot\033[m" << std::endl;
 		me->unequip(3);
 		me->unequip(5);
 
-		std::cout << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		delete rival;
 		delete me;
 		delete src;
 	}
 
-	std::cout << "\033[35;43mTest for deep copy a Character & a MaterialSource\033[m" << std::endl;
+	std::clog << "\033[35;43mTest for deep copy a Character & a MaterialSource\033[m" << std::endl;
 	{
 		Character* me = new Character("Cloud");
 		Character* rival = new Character("Sephiroth");
@@ -144,41 +144,41 @@ int	main()
 		tmp = src->createMateria("cure");
 		rival->equip(tmp);
 
-		std::cout << std::endl << "\033[33mCharacter\033[m" << std::endl;
-		std::cout << "\033[33mContent of the overridden\033[m" << std::endl;
+		std::clog << std::endl << "\033[33mCharacter\033[m" << std::endl;
+		std::clog << "\033[33mContent of the overridden\033[m" << std::endl;
 		rival->showMateria();
-		std::cout << "\033[33mOperation of copy constructor\033[m" << std::endl;
+		std::clog << "\033[33mOperation of copy constructor\033[m" << std::endl;
 		Character*	me2 = new Character(*me);
 		Character	me3(*me);
-		std::cout << "\033[33mOperation of copy assignment operator\033[m" << std::endl;
+		std::clog << "\033[33mOperation of copy assignment operator\033[m" << std::endl;
 		*rival = *me;
 
-		std::cout << "\033[33mContent of the original\033[m" << std::endl;
+		std::clog << "\033[33mContent of the original\033[m" << std::endl;
 		me->showMateria();
-		std::cout << "\033[33mContent of copy constructored\033[m" << std::endl;
+		std::clog << "\033[33mContent of copy constructored\033[m" << std::endl;
 		me2->showMateria();
 		me3.showMateria();
-		std::cout << "\033[33mContent of copy assigned\033[m" << std::endl;
+		std::clog << "\033[33mContent of copy assigned\033[m" << std::endl;
 		rival->showMateria();
 
-		std::cout << std::endl << "\033[33mMateriaSource\033[m" << std::endl;
-		std::cout << "\033[33mContent of the overridden\033[m" << std::endl;
+		std::clog << std::endl << "\033[33mMateriaSource\033[m" << std::endl;
+		std::clog << "\033[33mContent of the overridden\033[m" << std::endl;
 		dst->showMateria();
-		std::cout << "\033[33mOperation of copy constructor\033[m" << std::endl;
+		std::clog << "\033[33mOperation of copy constructor\033[m" << std::endl;
 		MateriaSource*	src2 = new MateriaSource(*src);
 		MateriaSource	src3(*src);
-		std::cout << "\033[33mOperation of copy assignment operator\033[m" << std::endl;
+		std::clog << "\033[33mOperation of copy assignment operator\033[m" << std::endl;
 		*dst = *src;
 
-		std::cout << "\033[33mContent of the original\033[m" << std::endl;
+		std::clog << "\033[33mContent of the original\033[m" << std::endl;
 		src->showMateria();
-		std::cout << "\033[33mContent of copy constructored\033[m" << std::endl;
+		std::clog << "\033[33mContent of copy constructored\033[m" << std::endl;
 		src2->showMateria();
 		src3.showMateria();
-		std::cout << "\033[33mContent of copy assigned\033[m" << std::endl;
+		std::clog << "\033[33mContent of copy assigned\033[m" << std::endl;
 		dst->showMateria();
 
-		std::cout << std::endl << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
+		std::clog << std::endl << "\033[33m ---- ---- ---- ----\033[m" << std::endl;
 		delete me;
 		delete me2;
 		delete rival;
